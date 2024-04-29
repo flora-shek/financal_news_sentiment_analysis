a1038f672d936c0c7aa73ddda819dd045ab9c02f
Shekinah Florance <84413589+flora-shek@users.noreply.github.com>
2024-04-29 14:32:37 +0000

    added api functionalities

---
+++ api.py
@@ -0,0 +1,16 @@
+import requests
+import os
+API_URL = "https://api-inference.huggingface.co/models/mrm8488/distilroberta-finetuned-financial-news-sentiment-analysis"
+api_key = os.environ['api_key']
+headers = {"Authorization": api_key}
+
+def query(payload):
+  response = requests.post(API_URL, headers=headers, json=payload)
+  return response.json()
+
+
+def api_loader(financial_text):
+  dict = {"inputs":financial_text}
+  txt = query(dict)
+  return txt
+  
--- app.py
+++ app.py
@@ -1,10 +1,15 @@
-from flask import Flask, render_template
-
+from flask import Flask, render_template,jsonify
+from api import api_loader
 app = Flask(__name__)
 @app.route("/")
 def hello():
     return render_template(
         "index.html")
 
+@app.route("/api/<financial_text>")
+def api(financial_text):
+  f_txt = api_loader(financial_text)
+  return jsonify(f_txt)
+  
 if __name__ == "__main__":
   app.run(host='0.0.0.0', port=4080,debug=True)
