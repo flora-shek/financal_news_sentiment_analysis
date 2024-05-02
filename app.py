f401d63f74184ed83938dcb3a278574f50e56c99
Shekinah Florance <84413589+flora-shek@users.noreply.github.com>
2024-05-02 15:11:44 +0000

    added some improvements

--- app.py
+++ app.py
@@ -24,10 +24,17 @@ def examples(example):
 def result():
   token = request.form.get('floatingTextarea')
   result = api_loader(token)
+  label1 = result[0][0]['label'].capitalize()
+  label2 =  result[0][1]['label'].capitalize()
+  label3 = result[0][2]['label'].capitalize()
   score1 = round((result[0][0]['score']*100),2)
   score2 = round((result[0][1]['score']*100),2)
   score3 = round((result[0][2]['score']*100),2)
-  return render_template("index.html",result = result,s1=score1,s2=score2,s3=score3)
+  return render_template("index.html",result = result,s1=score1,s2=score2,s3=score3,sentence = token,l1=label1,l2=label2,l3=label3)
+
+@app.route("/signin")
+def signin():
+  return render_template("signin.html")
   
 if __name__ == "__main__":
   app.run(host='0.0.0.0', port=4080,debug=True)
