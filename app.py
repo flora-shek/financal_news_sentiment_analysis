a0ef70b46ca8694ae767ba1a6a2f386bfaca8ea1
Shekinah Florance <84413589+flora-shek@users.noreply.github.com>
2024-05-03 07:14:22 +0000

    final signup form

--- app.py
+++ app.py
@@ -35,6 +35,10 @@ def result():
 @app.route("/signin")
 def signin():
   return render_template("signin.html")
+
+@app.route("/signup")
+def signup():
+  return render_template("signup.html")
   
 if __name__ == "__main__":
   app.run(host='0.0.0.0', port=4080,debug=True)
